17dd8b5ef36bca8d4811d70401fc1073fc6cd632
Erlend Tyrmi <erlend@tyrmi.com>
2019-04-05 11:33:05 +0200

    second commit

--- main.c
+++ main.c
@@ -2,28 +2,32 @@
 #include <stdbool.h>
 
 typedef struct line{
-    // Right (x,y)
-    double R_X;
-    double R_Y;
     // Left (x,y)
-    double L_X;
-    double L_Y;
+    double X1;
+    double Y1;
+    // Right (x,y)
+    double X2;
+    double Y2;
+    int drip;
+    int checked;
 } Line;
 
 int n;
 
 double getWidth(Line line){
-    return line.R_X - line.L_X;
+    return line.X2- line.X1;
 }
 
 double getCoveredPart(Line line, Line *lines){
     // returns the length of x values that are covered from above by another line
     double covered;
+    Line exposedLine = line;
     for (int i = 0; i < n; i++){
         Line otherLine = lines[i];
-        if (otherLine.R_X < line.R_X && otherLine.L_X > line.R_X){
+        if (otherLine.X1 < line.X1 && otherLine.X2 > line.X1){
             // return as soon as the whoe line is covered
-        } else if (line.R_X < otherLine.R_X && line.L_X > otherLine.R_X)){
+            // Add covered x range of not in range
+        } else if (line.X1 < otherLine.X1 && line.X2 > otherLine.X1){
 
         }
 
@@ -36,15 +40,15 @@ double getCoveredPart(Line line, Line *lines){
 }
 
 double getYofX(Line line, double x){
-    // Calculating ax + b based on R(x,y)
-    double a = (line.R_Y - line.L_Y) / (line.R_X - line.L_X);
-    double b = line.R_Y - a * line.R_X;
+    // Calculating ax + b based on Left coordinate(X1, Y1)
+    double a = (line.Y2 - line.Y1) / (line.X2 - line.X1);
+    double b = line.Y1 - a * line.X1;
     return a*x + b;
 }
 
 bool isLowerLine(Line low, Line high){
     // Find x value to test with. Takes highest right x. Works if x-values overlap, and lines never cross.
-    int x = (low.R_X < high.R_X? high.R_X : low.R_X);
+    int x = (low.X1 < high.X1? high.X1 : low.X1);
     return getYofX(low, x) < getYofX(high, x)? true : false;
 }
 
@@ -60,22 +64,35 @@ double getAccumulatedRain(Line line, Line lines[n]){
     return getRainSurface(line, lines) + getDrip(line, lines);
 }
 
+Line findHighestUncheckedLine(Line *lines){
+    Line highest = {0, 0, 0, 0, 0, 0};
+    for (int i = 0; i<n;i++){
+        if (lines[i].checked = 0)
+        if (isLowerLine(highest, lines[i])){
+            highest = lines[i];
+        }
+    }
+    return highest;
+}
+
 int main() {
 
-    printf("Hello! Please input the following info:\nNumber of lines to check, followed by enter.\n"
-           "For each line: four coordinates separated by spaces, followed by enter.\n->");
+    printf("Hello! This program prints the total water flow from each roof, represented by a line. "
+           "First: Please input the following info:\nNumber of lines to check, followed by enter.\n"
+           "For all subsequent lines: Four coordinates separated by spaces, followed by enter.\n->");
 
     // Get number of lines: n
     scanf("%d", &n);
     Line lines[n];
+
     // Make array of lines:
     for (int i = 0; i < n; i++){
-        scanf("%lf %lf %lf %lf", &lines[i].R_X, &lines[i].R_Y, &lines[i].L_X, &lines[i].L_Y);
+        scanf("%lf %lf %lf %lf %d %d", &lines[i].X1, &lines[i].Y1, &lines[i].X2, &lines[i].Y2, 0, 0);
     }
 
 
-    printf("TEST: Printing last element: Right coord: (%lf,%lf) Left coord: (%lf,%lf)\n",
-            lines[n-1].R_X, lines[n-1].R_Y, lines[n-1].L_X, lines[n-1].L_Y);
+    printf("TEST: Printing last element: Left coord: (%lf,%lf) Right coord: (%lf,%lf)\n",
+            lines[n-1].X1, lines[n-1].Y1, lines[n-1].X2, lines[n-1].Y2);
 
 
 
